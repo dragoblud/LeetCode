37d7d728917fe947fc22a8e71f0090c4c165fea9
Raunak Roy <raunakroy61@gmail.com>
2024-08-03 11:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (32.51%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        int n1 = v1.length(), n2 = v2.length();
+        for(int i=0,j=0;(i<n1)||(j<n2);i++,j++){
+            string s1 = "", s2 = "";
+            while(i<n1 && v1[i] != '.') s1+=v1[i++]; 
+            while(j<n2 && v2[j] != '.') s2+=v2[j++];
+            int x = 0, y = 0;
+            if(s1 != "") x = stoi(s1);
+            if(s2 != "") y = stoi(s2);
+            if(x<y) return -1;
+            else if(x>y) return 1;
+        }
+        return 0;
+    }
+};
