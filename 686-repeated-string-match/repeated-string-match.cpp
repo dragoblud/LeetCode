fc4727fe692cf381708659a57045474583e91c62
Raunak Roy <raunakroy61@gmail.com>
2024-08-12 21:44:49 +0530

    Time: 11 ms (38.62%) | Memory: 8.6 MB (66.48%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string s = a;
+        int res = 1;
+        int n = b.size()/a.size();
+        for(int i=0;i<=n+1;i++){
+            if(s.find(b) != string::npos) return res;
+                s+=a;
+                res++;
+        }
+        return -1;
+    }
+};
